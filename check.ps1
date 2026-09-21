131ba608e3e3289e955f090aad13e6c5168fdaae
David Vorel <davidvorel123@gmail.com>
2026-09-21 20:54:50 +0200

    chore: remove temp worker scripts

--- check.ps1
+++
@@ -1,5 +0,0 @@
-$content = [System.IO.File]::ReadAllText('styles.css')
-$open = [regex]::Matches($content, '\{').Count
-$close = [regex]::Matches($content, '\}').Count
-Write-Host "Open: $open, Close: $close"
-if ($open -eq $close) { exit 0 } else { exit 1 }
